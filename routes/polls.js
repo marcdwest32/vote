39019c448b83d4231949d3e5b1e02ad7dcf71431
Marc <marcdwest@gmail.com>
2019-10-08 17:58:30 -0500

    update poll not working. id wrong

--- routes/polls.js
+++ routes/polls.js
@@ -59,8 +59,35 @@ router.post(
 // @route PUT api/polls/:id
 // @desc Update poll
 // @access Private
-router.put('/:id', (req, res) => {
-	res.send('Update poll');
+router.put('/:id', auth, async (req, res) => {
+	const { question, option1, option2, option3 } = req.body;
+
+	// Poll object
+	const pollFields = {};
+	if (question) pollFields.question = question;
+	if (option1) pollFields.option1 = option1;
+	if (option2) pollFields.option2 = option2;
+	if (option3) pollFields.option3 = option3;
+
+	try {
+		let poll = await Poll.findById(req.params.id);
+
+		if (!poll) return res.status(404).json({ msg: 'Poll Not Found' });
+
+		// Make sure user owns poll
+		if (poll.user.toString() !== req.user.id) {
+			return res.status(401).json({ msg: 'Not Authorized ' });
+		}
+
+		poll = await Poll.findByIdAndUpdate(
+			req.params.id,
+			{ $set: pollFields },
+			{ new: true }
+		);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
 });
 
 // @route DELETE api/polls/:id
