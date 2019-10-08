8bad21641568c1fb606fe1543f1811b2cb05207b
Marc <marcdwest@gmail.com>
2019-10-08 15:56:19 -0500

    create poll schema

---
+++ models/Polls.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const PollSchema = mongoose.Schema({
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'users',
+	},
+	poll: {
+		type: String,
+		required: true,
+	},
+	option1: {
+		type: String,
+		required: true,
+	},
+	option2: {
+		type: String,
+		required: true,
+	},
+	option3: {
+		type: String,
+		required: true,
+	},
+	date: {
+		type: Date,
+		default: Date.now,
+	},
+});
+
+module.exports = mongoose.model('poll', PollSchema);
