76e6276b3ae71b39d9d9ceb3975eb0167690d1ea
Marc <marcdwest@gmail.com>
2019-10-08 17:22:24 -0500

    Post request to create poll

--- routes/polls.js
+++ routes/polls.js
@@ -21,9 +21,40 @@ router.get('/', auth, async (req, res) => {
 // @route POST api/polls
 // @desc Add new poll
 // @access Private
-router.post('/', (req, res) => {
-	res.send('Add poll');
-});
+router.post(
+	'/',
+	[
+		auth,
+		[
+			check('question', 'Poll Question is Required')
+				.not()
+				.isEmpty(),
+		],
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { question, option1, option2, option3 } = req.body;
+
+		try {
+			const newPoll = new Poll({
+				question,
+				option1,
+				option2,
+				option3,
+				user: req.user.id,
+			});
+			const poll = await newPoll.save();
+			res.json(poll);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server Error');
+		}
+	}
+);
 
 // @route PUT api/polls/:id
 // @desc Update poll
