ff1c736edb3f4728b79af6f25883b436d27ee616
Marc <marcdwest@gmail.com>
2019-10-09 17:13:25 -0500

    add PollForm to Home

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Polls from '../polls/Polls';
+import PollForm from '../polls/PollForm';
 
 const Home = () => {
 	return (
 		<div className='grid-2'>
 			<div>
-				{/* Poll Form? */}
+				<PollForm />
 			</div>
 			<div>
 				<Polls />
