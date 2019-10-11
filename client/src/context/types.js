8be3eea291d90864042c1c8f4697e79bf33ace4c
Marc <marcdwest@gmail.com>
2019-10-11 14:55:34 -0500

    clearfilter

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -3,6 +3,6 @@ export const DELETE_POLL = 'DELETE_POLL';
 export const SET_CURRENT = 'SET_CURRENT';
 export const UPDATE_POLL = 'UPDATE_POLL';
 export const FILTER_POLLS = 'FILTER_POLLS';
-export const CLEAR_POLLS = 'CLEAR_POLLS';
+export const CLEAR_FILTER = 'CLEAR_FILTER';
 export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
