0ccc6dc61d4f34a95b198db9b8d3df9472433f6e
Marc <marcdwest@gmail.com>
2019-10-15 11:35:51 -0500

    add alerts

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
-import PollState from './context/poll/PollState';
-import AuthState from './context/auth/AuthState';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alerts from './components/layout/Alerts';
+import PollState from './context/poll/PollState';
+import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
 import setAuthToken from './utils/setAuthToken';
 import './App.css';
 
@@ -18,19 +20,22 @@ const App = () => {
 	return (
 		<AuthState>
 			<PollState>
-				<Router>
-					<Fragment className='App'>
-						<Navbar />
-						<div className='container'>
-							<Switch>
-								<Route exact path='/' component={Home} />
-								<Route exact path='/about' component={About} />
-								<Route exact path='/register' component={Register} />
-								<Route exact path='/login' component={Login} />
-							</Switch>
-						</div>
-					</Fragment>
-				</Router>
+				<AlertState>
+					<Router>
+						<Fragment className='App'>
+							<Navbar />
+							<div className='container'>
+								<Alerts />
+								<Switch>
+									<Route exact path='/' component={Home} />
+									<Route exact path='/about' component={About} />
+									<Route exact path='/register' component={Register} />
+									<Route exact path='/login' component={Login} />
+								</Switch>
+							</div>
+						</Fragment>
+					</Router>
+				</AlertState>
 			</PollState>
 		</AuthState>
 	);
