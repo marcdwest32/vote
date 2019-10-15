b84560b89571b538332accbe25081f2885bb2d4d
Marc <marcdwest@gmail.com>
2019-10-15 13:56:20 -0500

    load user

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import Polls from '../polls/Polls';
 import PollForm from '../polls/PollForm';
 import PollFilter from '../polls/PollFilter';
+import AuthContext from '../../context/auth/authContext';
 
 const Home = () => {
+	const authContext = useContext(AuthContext);
+
+	useEffect(() => {
+		authContext.loadUser();
+		// eslint-disable-next-line
+	}, []);
+
 	return (
 		<div className='grid-2'>
 			<div>
