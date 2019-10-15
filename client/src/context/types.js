20583b6ee57d053878e7b378f9c6c0982b6526d0
Marc <marcdwest@gmail.com>
2019-10-14 20:46:07 -0500

    VoteError

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -6,3 +6,4 @@ export const FILTER_POLLS = 'FILTER_POLLS';
 export const CLEAR_FILTER = 'CLEAR_FILTER';
 export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
+export const VOTE_ERROR = '	VOTE_ERROR';
