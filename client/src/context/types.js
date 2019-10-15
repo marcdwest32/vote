33443e7a95f558505b7da843d9ac720a2c91069b
Marc <marcdwest@gmail.com>
2019-10-15 16:00:44 -0500

    POLL_ERROR case

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -1,4 +1,5 @@
 export const ADD_POLL = 'ADD_POLL';
+export const POLL_ERROR = 'POLL_ERROR';
 export const DELETE_POLL = 'DELETE_POLL';
 export const SET_CURRENT = 'SET_CURRENT';
 export const UPDATE_POLL = 'UPDATE_POLL';
