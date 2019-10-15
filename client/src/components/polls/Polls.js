9da84686de8eec101a77efe1f7cd1c15ed8b7170
Marc <marcdwest@gmail.com>
2019-10-15 17:17:05 -0500

    getPolls

--- client/src/components/polls/Polls.js
+++ client/src/components/polls/Polls.js
@@ -1,32 +1,42 @@
-import React, { Fragment, useContext } from 'react';
+import React, { Fragment, useContext, useEffect } from 'react';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import PollItem from './PollItem';
 import PollContext from '../../context/poll/pollContext';
+import Spinner from '../layout/Spinner';
 
 const Polls = () => {
 	const pollContext = useContext(PollContext);
 
-	const { polls, filtered } = pollContext;
+	const { polls, filtered, getPolls, loading } = pollContext;
 
-	if (polls.length === 0) {
+	useEffect(() => {
+		getPolls();
+		// eslint-disable-next-line
+	}, []);
+
+	if (polls !== null && polls.length === 0 && !loading) {
 		return <h4>Please Post a Question</h4>;
 	}
 
 	return (
 		<Fragment>
-			<TransitionGroup>
-				{filtered !== null
-					? filtered.map(poll => (
-							<CSSTransition key={poll._id} timeout={500} classNames='item'>
-								<PollItem poll={poll} />
-							</CSSTransition>
-					  ))
-					: polls.map(poll => (
-							<CSSTransition key={poll._id} timeout={500} classNames='item'>
-								<PollItem poll={poll} />
-							</CSSTransition>
-					  ))}
-			</TransitionGroup>
+			{polls !== null && !loading ? (
+				<TransitionGroup>
+					{filtered !== null
+						? filtered.map(poll => (
+								<CSSTransition key={poll._id} timeout={500} classNames='item'>
+									<PollItem poll={poll} />
+								</CSSTransition>
+						  ))
+						: polls.map(poll => (
+								<CSSTransition key={poll._id} timeout={500} classNames='item'>
+									<PollItem poll={poll} />
+								</CSSTransition>
+						  ))}
+				</TransitionGroup>
+			) : (
+				<Spinner />
+			)}
 		</Fragment>
 	);
 };
